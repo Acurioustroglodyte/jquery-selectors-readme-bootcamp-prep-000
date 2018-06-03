be611826aa068c669034386dab7dc53bdef68e70
Trevor Jameson <tjameson15@gmail.com>
2018-06-03 12:41:37 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  $('p');
+}
+
+function lastImageSelector() {
+  $('img:last-child');
+}
+
+function ninjaBabySelector() {
+  $('#baby-ninja');
+}
+
+function divSelector() {
+  $('div.pics');
+}
+
+function firstListItem() {
+  $('ul#pic-list li:first-child');
+}
